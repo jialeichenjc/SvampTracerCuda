058a0178468f326d91e5384cafc8a34eb320e43f
Joyce Chen <superbrainchen@gmail.com>
2019-01-14 23:22:52 -0800

    Add cuda rand() in main

--- main.cu
+++ main.cu
@@ -32,26 +32,54 @@ void check_cuda(cudaError_t result, char const *const func, const char *const fi
     }
 }
 
-__device__ vec3 color(const ray &r, hitable * world, int depth) {
-    hit_record rec;
-    // t_min set to 0.001 to avoid the shadow acne problem
-    if (world->hit(r, 0.001, FLT_MAX, rec)) {
-        ray scattered;
-        vec3 attenuation;
-        if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
-            return attenuation*color(scattered, world, depth + 1);
+__device__ vec3 color(const ray &r, hitable ** world, curandState * local_rand_state) {
+    ray cur_ray = r;
+    vec3 cur_attenuation = vec3(1.0, 1.0, 1.0);
+
+    for (int i = 0; i < 50; i++) {
+        hit_record rec;
+        if ((*world)->hit(cur_ray, 0.001f, FLT_MAX, rec)) {
+            ray scattered;
+            vec3 attenuation;
+            if (rec.mat_ptr->scatter(cur_ray, rec, attenuation, scattered, local_rand_state)) {
+                cur_attenuation *= attenuation;
+                cur_ray = scattered;
+            }
+            else {
+                return vec3(0, 0, 0);
+            }
         }
         else {
-            return vec3(0, 0, 0);
+            vec3 unit_direction = unit_vector(cur_ray.direction());
+            vec3 unit_direction = unit_vector(r.direction());
+            float t = 0.5 * (unit_direction.y() + 1.0); // 0 <= t <= 1
+            // lerp between white and blue
+            // lerp := (1-t) * start_value + t * end_value;
+            vec3 c = (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+            return cur_attenuation * c;
         }
     }
-    else {
-        vec3 unit_direction = unit_vector(r.direction());
-        float t = 0.5 * (unit_direction.y() + 1.0); // 0 <= t <= 1
-                                                    // lerp between white and blue
-                                                    // lerp := (1-t) * start_value + t * end_value;
-        return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+    return vec3(0.0, 0.0, 0.0);
+}
+
+__global__ void rand_init(curandState * rand_state) {
+    if (threadIdx.x == 0 && blockIdx.x == 0) {
+        curand_init(1984, 0, 0, rand_state);
     }
+}
+
+__global__ void render_init(int max_x, int max_y, curandState * rand_state) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if((i >= max_x) || (j >= max_y)) return;
+    int pixel_index = j*max_x + i;
+    // Each thread gets same seed, a different sequence number, no offset
+    curand_init(1984, pixel_index, 0, &rand_state[pixel_index]);
+}
+
+__global__ void render(vec3 *fb, int max_x, int max_y, int ns, camera ** cam, hitable ** world, curandState * randState) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
 }
 
 hitable * random_scene() {
