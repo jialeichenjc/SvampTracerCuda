1d3fdf1b8d41a8ef91f9e8e39a273f6a527507c2
Joyce Chen <superbrainchen@gmail.com>
2019-01-15 23:45:49 -0800

    Modify render function to run on device

--- main.cu
+++ main.cu
@@ -77,9 +77,26 @@ __global__ void render_init(int max_x, int max_y, curandState * rand_state) {
     curand_init(1984, pixel_index, 0, &rand_state[pixel_index]);
 }
 
-__global__ void render(vec3 *fb, int max_x, int max_y, int ns, camera ** cam, hitable ** world, curandState * randState) {
+// __global__ called from host, executed on device
+__global__ void render(vec3 *fb, int max_x, int max_y, int ns, camera ** cam, hitable ** world, curandState * rand_state) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if((i >= max_x) || (j >= max_y)) return;
+    int pixel_index = j*max_x + i;
+    curandState local_rand_state = rand_state[pixel_index];
+    vec3 col(0, 0, 0);
+    for (int s = 0; s < ns; s++) {
+        float u = float(i + curand_uniform(&local_rand_state)) / float(max_x);
+        float v = float(j + curand_uniform(&local_rand_state)) / float(max_y);
+        ray r = (*cam)->get_ray(u, v, &local_rand_state);
+        col += color(r, world, &local_rand_state);
+    }
+    rand_state[pixel_index] = local_rand_state;
+    col /= float(ns);
+    col[0] = sqrt(col[0]);
+    col[1] = sqrt(col[1]);
+    col[2] = sqrt(col[2]);
+    fb[pixel_index] = col;
 }
 
 hitable * random_scene() {
